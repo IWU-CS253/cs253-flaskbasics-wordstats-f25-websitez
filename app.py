65e94934fa56e69277698bc620c8992622d7542e
Cass-Adams <cadams@iwu.edu>
2025-09-24 15:47:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,11 @@ def register():
     data_length = len(data)
     word_list = data.split()
     word_count = len(word_list)
+    total_word_len = 0
+    avg_word_len = 0
+    for word in word_list:
+        total_word_len = total_word_len + len(word)
+    avg_word_len = total_word_len / word_count
 
     reverse_data = data[::-1]
-    return render_template("analyze.html", data = data, word_count=word_count, data_length = data_length, word_list=word_list, reverse_data=reverse_data)
+    return render_template("analyze.html", data = data, word_count=word_count, data_length = data_length, word_list=word_list, reverse_data=reverse_data, avg_word_len=avg_word_len)
